1269b385c65ce8cb073172ed31f5302adc8e3111
lAngelP <info@langelp.net>
2017-04-09 22:10:16 +0200

    .

--- SQL/drop_tables.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE goles;
-DROP TABLE empates;
-DROP TABLE otros_nombres;
-DROP TABLE partido;
-DROP TABLE residir;
-DROP TABLE jugar;
-DROP TABLE estadio;
-DROP TABLE temporada;
-DROP TABLE equipo;
