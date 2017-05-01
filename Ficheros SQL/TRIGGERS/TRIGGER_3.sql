9102291027e717225bce3dacd6a19c34d374b3ec
aznarjorge <jorgeaznar_97@hotmail.com>
2017-05-01 22:01:15 +0200

    trigger_error

--- Ficheros SQL/TRIGGERS/TRIGGER_3.sql
+++ Ficheros SQL/TRIGGERS/TRIGGER_3.sql
@@ -1,37 +1,41 @@
-DROP TABLE actores_directores;
+DROP TABLE act_directores;
 DROP TRIGGER act_actores_directores;
 DROP TRIGGER dir_actores_directores;
 
 CREATE TABLE act_directores (
 		actor_director INTEGER REFERENCES Persona(ID) ON DELETE CASCADE ,
-		pelicula INTEGER REFERENCES PELICULA(ID) ON DELETE CASCADE,
+		pelicula INTEGER REFERENCES PELICULA(ID),
     PRIMARY KEY (actor_director,pelicula)
 );
 
 CREATE TRIGGER act_actores_directores
-AFTER INSERT
+	AFTER INSERT
 	ON actor_pelicula
 	FOR EACH ROW
-		DECLARE
-		PRAGMA AUTONOMOUS_TRANSACTION;
+	DECLARE
+	director INTEGER;
+	peli INTEGER;
 BEGIN
-	  SELECT persona AS director FROM DIRECTOR_PELICULA WHERE DIRECTOR_PELICULA.pelicula  =: NEW.PELICULA;
-    IF director = :NEW.PERSONA THEN
-      INSERT INTO act_directores (actor_director, pelicula) VALUES (:NEW.persona, :NEW.PELICULA);
-    END IF;
+	SELECT persona INTO director FROM director_pelicula WHERE director_pelicula.persona  = :NEW.persona;
+	SELECT pelicula INTO peli FROM pelicula WHERE pelicula.id  = :NEW.pelicula;
+    	IF director = :NEW.PERSONA THEN
+      		INSERT INTO act_directores (actor_director, pelicula) VALUES (:NEW.persona, :NEW.PELICULA);
+    	END IF;
 END;
 /
 
 CREATE TRIGGER dir_actores_directores
-AFTER INSERT
+	AFTER INSERT
 	ON director_pelicula
 	FOR EACH ROW
-		DECLARE
-		PRAGMA AUTONOMOUS_TRANSACTION;
+	DECLARE
+	actor INTEGER;
+	pel INTEGER;
 BEGIN
-    SELECT persona AS actor FROM ACTOR_PELICULA WHERE ACTOR_PELICULA.pelicula  =: NEW.PELICULA;
-    IF actor = :NEW.PERSONA THEN
-      INSERT INTO act_directores (actor_director, pelicula) VALUES (:NEW.persona, :NEW.PELICULA);
-    END IF;
+    	SELECT persona INTO actor FROM ACTOR_PELICULA WHERE ACTOR_PELICULA.persona  = :NEW.persona;
+    	SELECT pelicula INTO pel FROM pelicula WHERE pelicula.id  = :NEW.pelicula;
+   	IF actor = :NEW.PERSONA THEN
+      		INSERT INTO act_directores (actor_director, pelicula) VALUES (:NEW.persona, :NEW.PELICULA);
+    	END IF;
 END;
 /
