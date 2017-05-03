10073bdf87ba3e1fceaadf00102630934a482f30
aznarjorge <jorgeaznar_97@hotmail.com>
2017-05-03 23:32:38 +0200

    Update QUERY_3_opt.sql

--- Ficheros SQL/QUERIES/PROFESORES/QUERY_3_opt.sql
+++ Ficheros SQL/QUERIES/PROFESORES/QUERY_3_opt.sql
@@ -1,22 +1,22 @@
--- Tercera consulta obligatoria: 	Directores para los cuales la última obra en la que han participado ha sido como
+-- Tercera consulta obligatoria: 	Directores para los cuales la Ãºltima obra en la que han participado ha sido como
 -- 									actor/actriz
 
--- Esta view obtiene todas las películas de cada director así como su fecha de estreno necesario para 
--- posteriormente saber cual es su última película
+-- Esta view obtiene todas las pelÃ­culas de cada director asÃ­ como su fecha de estreno necesario para 
+-- posteriormente saber cual es su Ãºltima pelÃ­cula
 
 CREATE VIEW PELICULAS_DIRECTOR AS
     SELECT persona as director, id, fecha_de_estreno
     FROM director_pelicula
     INNER JOIN pelicula ON director_pelicula.pelicula = pelicula.id;
 	
--- Esta view obtiene la fecha de estreno de la última película de cada director a partir de la view anterior
+-- Esta view obtiene la fecha de estreno de la Ãºltima pelÃ­cula de cada director a partir de la view anterior
 
 CREATE VIEW DIR_ULTIMAS_PELICULAS AS
 	SELECT PELICULAS_DIRECTOR.director, MAX(fecha_de_estreno) AS ultima_pelicula
 	FROM PELICULAS_DIRECTOR
 	GROUP BY PELICULAS_DIRECTOR.director;
 	
--- Esta view asocia a cada director la última película que dirigió (la que coincide con la fecha de la view anterior=)
+-- Esta view asocia a cada director la Ãºltima pelÃ­cula que dirigiÃ³ (la que coincide con la fecha de la view anterior=)
 
 CREATE VIEW DIRECTOR_Y_PELICULA AS
 	SELECT id, DIR_ULTIMAS_PELICULAS.director
@@ -24,9 +24,9 @@ CREATE VIEW DIRECTOR_Y_PELICULA AS
 	INNER JOIN  PELICULAS_DIRECTOR ON 	DIR_ULTIMAS_PELICULAS.ultima_pelicula = PELICULAS_DIRECTOR.FECHA_DE_ESTRENO AND
 										DIR_ULTIMAS_PELICULAS.director = PELICULAS_DIRECTOR.director;
 
--- Esta view obtiene los actores que coinciden con las últimas películas de cada director y que participan como directores
+-- Esta view obtiene los actores que coinciden con las Ãºltimas pelÃ­culas de cada director y que participan como directores
 
-CREATE VIEW ACTORES_DIRECTORES AS
+CREATE VIEW ACTORES_DIRECTORES_TRIGGER AS
 	SELECT director
 	FROM DIRECTOR_Y_PELICULA
 	INNER JOIN  ACT_DIRECTORES ON actor_director = director AND pelicula = director_y_pelicula.id;																			
