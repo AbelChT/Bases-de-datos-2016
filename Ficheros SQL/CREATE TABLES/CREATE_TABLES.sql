9df61c64357b162ea82d0b8016a7407c6cf234b5
Abel <abelcht@outlook.es>
2017-05-14 15:47:38 +0200

    int

--- Ficheros SQL/CREATE TABLES/CREATE_TABLES.sql
+++ Ficheros SQL/CREATE TABLES/CREATE_TABLES.sql
@@ -1,98 +1,102 @@
--- Almacena los aeropuertos, su IATA, sus coordenadas, y su ciudad de ubicación
+-- Almacena los aeropuertos, su IATA, sus coordenadas, y su ciudad de ubicacion
 CREATE TABLE aeropuerto (
-    IATA VARCHAR(3) PRIMARY KEY,
+	iata     VARCHAR(3) PRIMARY KEY,
+  oaci VARCHAR(4) UNIQUE,
+	nombre VARCHAR(40),
 	longitud FLOAT,
-	latitud	FLOAT,
-    ciudad VARCHAR(50) REFERENCES ciudad(id) ON DELETE CASCADE,
+	latitud  FLOAT,
+	ciudad   INTEGER REFERENCES ciudad (id) ON DELETE SET NULL
 );
 
 -- Almacena las ciudades con su correspondiente nombre,
--- estado y país
+-- estado y pais
 CREATE TABLE ciudad (
-    id INTEGER PRIMARY KEY,
-    nombre VARCHAR(50),
-    estado VARCHAR(50),
-    pais VARCHAR(50)
+	id     INTEGER PRIMARY KEY,
+	nombre VARCHAR(50) NOT NULL,
+	estado VARCHAR(50) NOT NULL,
+	pais   VARCHAR(50) NOT NULL,
+  UNIQUE (nombre,estado,pais)
 );
 
--- Almacena los datos de cada vuelo (físico) realizado, su numero
--- fecha, aerolínea que lo organiza, destino y origen, etcétera
-CREATE TABLE vuelo (
-  	id INTEGER PRIMARY KEY,
-   	numero_vuelo INTEGER,
-   	fecha DATE,
-	avion VARCHAR(5) REFERENCES avion(tailname) ON DELETE CASCADE, 
-   	aerolinea VARCHAR(2) REFERENCES aerolinea(iata) ON DELETE CASCADE,
-   	destino VARCHAR(3) REFERENCES aeropuerto(iata) ON DELETE CASCADE,
-   	origen VARCHAR(3) REFERENCES aeropuerto(iata) ON DELETE CASCADE,
-	hora_salida_programada INTEGER,
-	hora_salida_real	INTEGER,
-	hora_despegue	INTEGER,
-	hora_aterrizaje	INTEGER,
-	hora_llegada_programada INTEGER,
-	hora_llegada_real	INTEGER,
-	distancia	FLOAT
+-- Almacena los datos de cada avion, es decir, su tailname,
+-- el modelo del avion, etcetera
+--(Se permiten aviones sin modelo)
+CREATE TABLE avion (
+  id INTEGER PRIMARY KEY,
+	numero_de_cola VARCHAR(5) NOT NULL,
+	modelo   INTEGER REFERENCES modelo (id) ON DELETE SET NULL,
+  fecha_de_registro DATE,
+	UNIQUE (numero_de_cola,fecha_de_registro)
 );
 
--- Almacena los datos de cada avión, es decir, su tailname,
--- el modelo del avión, etcétera
-CREATE TABLE  avion (
-	tailname VARCHAR(5) PRIMARY KEY,	
-	modelo INTEGER REFERENCES modelo(id) ON DELETE CASCADE,  
-	-- otros campos
-);
-
--- Almacena los diferentes modelos que existen de aviones, así como su
+-- Almacena los diferentes modelos que existen de aviones, asi como su
 -- nombre y el fabricante de cada modelo
-CREATE TABLE  modelo (
-  	id INTEGER PRIMARY KEY,
-   	nombre VARCHAR(50),
-	fabricante VARCHAR(100)
-	-- otros campos
+CREATE TABLE modelo (
+	id         INTEGER PRIMARY KEY,
+	nombre     VARCHAR(50) NOT NULL,
+	fabricante VARCHAR(100) NOT NULL,
+  tipo_motor VARCHAR(50),
+  tipo_avion VARCHAR(50),
+  UNIQUE (nombre,fabricante)
 );
 
--- Relaciona cada vuelo con el avión que lo realiza
---CREATE TABLE avion_vuelo (
---    vuelo INTEGER REFERENCES vuelo(id) ON DELETE CASCADE,
---    avion VARCHAR(5) REFERENCES avion(tailname) ON DELETE CASCADE,
---    PRIMARY KEY (vuelo,avion)
---);
-
--- Almacena las aerolíneas con su nombre y su IATA correspondiente
+-- Almacena las aerolÃ­neas con su nombre y su IATA correspondiente
 CREATE TABLE aerolinea (
-	iata VARCHAR(2) PRIMARY KEY,
-	nombre VARCHAR(100)
+	iata   VARCHAR(2) PRIMARY KEY,
+	nombre VARCHAR(100),
+  fundacion DATE,
+  sede INTEGER REFERENCES ciudad(id) ON DELETE SET NULL
 );
 
--- Relaciona cada vuelo con su codigo de cancelación
-CREATE TABLE vuelos_cancelados (
-	vuelo INTEGER REFERENCES vuelo(id) ON DELETE CASCADE,
+
+
+
+-- Relaciona cada vuelo con su codigo de cancelaciï¿½n
+CREATE TABLE vuelos_cancelados (--- Revisar
+	vuelo  INTEGER REFERENCES vuelo (id) ON DELETE CASCADE,
 	codigo INTEGER,
-	PRIMARY KEY(vuelo)
+	PRIMARY KEY (vuelo,codigo)
 );
 
 -- Relaciona cada vuelo con su el tipo/s de retraso que ha sufrido (causas)
 CREATE TABLE vuelos_retrasados (
-	vuelo INTEGER REFERENCES vuelo(id) ON DELETE CASCADE,
-	tipo VARCHAR(20),
---	tipos: por la compañia, por el temporal, por problemas con la seguridad,
---	por problemas de seguridad del gobierno, por retraso en la llegada del avión
-	PRIMARY KEY(vuelo,tipo)
-); 
+	vuelo INTEGER REFERENCES vuelo (id) ON DELETE CASCADE,
+	tipo  VARCHAR(20),
+	--	tipos: por la compaï¿½ia, por el temporal, por problemas con la seguridad,
+	--	por problemas de seguridad del gobierno, por retraso en la llegada del aviï¿½n
+	PRIMARY KEY (vuelo, tipo)
+);
 
 -- Relaciona cada vuelo con las escalas de emergencia que ha tenido
 CREATE TABLE escalas (
-	vuelo INTEGER REFERENCES vuelo(id) ON DELETE CASCADE,
-	avion VARCHAR(5) REFERENCES avion(tailname) ON DELETE CASCADE, 
-   	aerolinea VARCHAR(2) REFERENCES aerolinea(iata) ON DELETE CASCADE,
-   	aeropuerto_escala VARCHAR(3) REFERENCES aeropuerto(iata) ON DELETE CASCADE,
-	hora_despegue	INTEGER,
-	hora_aterrizaje	INTEGER,
-	distancia_adiccional	FLOAT,
-	PRIMARY KEY(vuelo,hora_despegue)
+	vuelo                INTEGER REFERENCES vuelo (id) ON DELETE CASCADE,
+	avion                VARCHAR(5) REFERENCES avion (tailname) ON DELETE CASCADE,
+	aerolinea            VARCHAR(2) REFERENCES aerolinea (iata) ON DELETE CASCADE,
+	aeropuerto_escala    VARCHAR(3) REFERENCES aeropuerto (iata) ON DELETE CASCADE,
+	hora_despegue        INTEGER,
+	hora_aterrizaje      INTEGER,
+	distancia_adiccional FLOAT,
+	PRIMARY KEY (vuelo, hora_despegue)
 );
 
-
+-- Almacena los datos de cada vuelo (fisico) realizado, su numero
+-- fecha, aerolinea que lo organiza, destino y origen, etcetera
+CREATE TABLE vuelo (
+	id                      INTEGER PRIMARY KEY,
+	numero_vuelo            INTEGER,
+	fecha                   DATE,
+	avion                   INTEGER REFERENCES avion (id) ON DELETE SET NULL,-- Avion que realiza el vuelo
+	aerolinea               VARCHAR(2) REFERENCES aerolinea (iata) ON DELETE SET NULL,
+	destino                 VARCHAR(3) REFERENCES aeropuerto (iata) ON DELETE SET NULL,
+	origen                  VARCHAR(3) REFERENCES aeropuerto (iata) ON DELETE SET NULL,
+	hora_salida_programada  INTEGER,
+	hora_salida_real        INTEGER,
+	hora_despegue           INTEGER,
+	hora_aterrizaje         INTEGER,
+	hora_llegada_programada INTEGER,
+	hora_llegada_real       INTEGER,
+	distancia               FLOAT
+);
 
 
 
