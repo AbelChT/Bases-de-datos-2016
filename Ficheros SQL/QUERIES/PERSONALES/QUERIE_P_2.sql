49df1c228f515ed20f912cc4c05958ea8837c44a
Abel <abelcht@outlook.es>
2017-05-16 12:40:30 +0200

    profesores+personal

--- Ficheros SQL/QUERIES/PERSONALES/QUERIE_P_2.sql
+++ Ficheros SQL/QUERIES/PERSONALES/QUERIE_P_2.sql
@@ -1,5 +1,6 @@
 -- Modelo de avión que más distancia ha recorrido
 
+-- Sumas parciales distancia recorrida en emergencia y en vuelo normal
 CREATE VIEW DISTANCIA_AVION AS
 (SELECT AVION,sum(DISTANCIA) AS DISTANCIA
 FROM VUELO
@@ -8,4 +9,26 @@ UNION ALL
 (SELECT AVION, sum(DISTANCIA_ADICCIONAL) AS DISTANCIA
 FROM ESCALAS_EMERGENCIAS
 GROUP BY AVION);
+
+-- Distancia recorrida por cada avion
+CREATE VIEW DISTANCIA_AVION_SUM AS
+SELECT avion,sum(distancia) AS distancia
+FROM DISTANCIA_AVION
+GROUP BY avion;
+
+CREATE VIEW DISTANCIA_MODELO AS
+SELECT  modelo, sum(distancia) AS distancia
+FROM DISTANCIA_AVION_SUM
+INNER JOIN AVION ON DISTANCIA_AVION_SUM.AVION=AVION.ID
+GROUP BY modelo;
+
+SELECT modelo
+FROM DISTANCIA_MODELO
+WHERE distancia IN (
+  SELECT max(distancia) as maxima_distancia
+  FROM DISTANCIA_MODELO
+);
+
+
+
 
