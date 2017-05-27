ebd95a8b44c8f0b75b2a91c56bb184115631b332
Abel <abelcht@outlook.es>
2017-05-27 16:05:17 +0200

    mojara querie3

---
+++ Ficheros SQL/QUERIES/PROFESORES/QUERY_3_buena.sql
@@ -0,0 +1,18 @@
+-- Contemplamos como un trayecto ir de A a B y otro diferente ir de B a A
+CREATE VIEW vuelos_aerolinea AS
+	SELECT DISTINCT aerolinea, destino, origen 
+	FROM vuelo;
+
+SELECT A.NOMBRE, B.NOMBRE, ((coincidencias/numero_vuelos)*100) AS porcent
+FROM( SELECT a.aerolinea AS aerolinea_ppal, b.aerolinea  AS aerolinea_sec, COUNT(*) as coincidencias
+	    FROM vuelos_aerolinea a
+	    INNER JOIN vuelos_aerolinea b ON a.destino=b.destino AND a.origen=b.origen AND a.aerolinea!=b.aerolinea
+	    GROUP BY (a.AEROLINEA,b.AEROLINEA))
+INNER JOIN( SELECT aerolinea as aerolinea_contar, COUNT(*) as numero_vuelos
+	          FROM vuelos_aerolinea
+	          GROUP BY aerolinea) ON aerolinea_contar = aerolinea_ppal AND ((coincidencias/numero_vuelos)>=0.3)
+INNER JOIN AEROLINEA A ON aerolinea_ppal=A.IATA
+INNER JOIN AEROLINEA B ON aerolinea_sec=B.IATA
+ORDER BY porcent DESC;
+
+DROP VIEW vuelos_aerolinea;
