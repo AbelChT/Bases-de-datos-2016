ea1db194d500f2faf1bc676b4a05b9f19169d4fe
lAngelP <info@langelp.net>
2017-05-28 22:26:56 +0200

    Update QUERY_1.sql

--- Ficheros SQL/QUERIES/PROFESORES/QUERY_1.sql
+++ Ficheros SQL/QUERIES/PROFESORES/QUERY_1.sql
@@ -3,11 +3,11 @@ SELECT COUNT(*) num, estado FROM (
 	INNER JOIN VUELO v ON v.ID = r.vuelo
 	INNER JOIN aeropuerto a1 ON a1.IATA = v.ORIGEN
 	INNER JOIN ciudad c1 ON a1.ciudad = c1.id
-	INNER JOIN avion a ON v.avion = a.id)
+	WHERE EXISTS(SELECT id FROM escalas_emergencias WHERE vuelo = v.id))
 	UNION ALL
 	(SELECT estado FROM VUELOS_RETRASADOS r
 	INNER JOIN VUELO v ON v.ID = r.vuelo
 	INNER JOIN aeropuerto a1 ON a1.IATA = v.DESTINO
 	INNER JOIN ciudad c1 ON a1.ciudad = c1.id
-	INNER JOIN avion a ON v.avion = a.id)
+	WHERE EXISTS(SELECT id FROM escalas_emergencias WHERE vuelo = v.id))
 ) GROUP BY estado;
