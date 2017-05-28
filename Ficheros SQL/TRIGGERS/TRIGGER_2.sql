e9c7661746e6f8d6f35f68e0e8c72087eee5d0f4
Abel <abelcht@outlook.es>
2017-05-28 12:30:44 +0200

    falta_update_delete_trigger

--- Ficheros SQL/TRIGGERS/TRIGGER_2.sql
+++ Ficheros SQL/TRIGGERS/TRIGGER_2.sql
@@ -1,27 +1,39 @@
 -- Se utiliza para almacenar los aviones que operan en cada aeropuerto
 CREATE TABLE aviones_aereopuerto (
-  avion INTEGER REFERENCES AVION(ID),
-  aeropuerto VARCHAR(4) REFERENCES AEROPUERTO(IATA),
+  avion INTEGER REFERENCES AVION(ID) ON DELETE CASCADE,
+  aeropuerto VARCHAR(4) REFERENCES AEROPUERTO(IATA) ON DELETE CASCADE,
   PRIMARY KEY (avion,aeropuerto)
 );
 
--- Falta implementar el trigger
 CREATE OR REPLACE TRIGGER tr_aviones_aereopuerto_insert
   AFTER INSERT ON VUELO
   FOR EACH ROW
+  DECLARE
+    numero INTEGER;
   BEGIN
-    IF NOT EXISTS( SELECT *
-                    FROM aviones_aereopuerto
-                    WHERE :NEW.AVION=aviones_aereopuerto.avion AND :NEW.ORIGEN=aviones_aereopuerto.aeropuerto)
-    THEN
-      INSERT INTO aviones_aereopuerto(avion,aeropuerto) VALUES (:NEW.AVION,:NEW.ORIGEN);
+
+    IF :NEW.ORIGEN IS NOT NULL
+      THEN
+      SELECT count(*) INTO numero
+      FROM aviones_aereopuerto
+      WHERE :NEW.AVION=aviones_aereopuerto.avion AND :NEW.ORIGEN=aviones_aereopuerto.aeropuerto;
+
+      IF (numero = 0)
+      THEN
+        INSERT INTO aviones_aereopuerto(avion,aeropuerto) VALUES (:NEW.AVION,:NEW.ORIGEN);
+      END IF;
     END IF;
 
-    IF NOT EXISTS( SELECT *
-                    FROM aviones_aereopuerto
-                    WHERE :NEW.AVION=aviones_aereopuerto.avion AND :NEW.DESTINO=aviones_aereopuerto.aeropuerto)
-    THEN
-      INSERT INTO aviones_aereopuerto(avion,aeropuerto) VALUES (:NEW.AVION,:NEW.DESTINO);
+    IF :NEW.DESTINO IS NOT NULL
+      THEN
+      SELECT count(*) INTO numero
+      FROM aviones_aereopuerto
+      WHERE :NEW.AVION=aviones_aereopuerto.avion AND :NEW.DESTINO=aviones_aereopuerto.aeropuerto;
+
+      IF (numero = 0)
+      THEN
+        INSERT INTO aviones_aereopuerto(avion,aeropuerto) VALUES (:NEW.AVION,:NEW.DESTINO);
+      END IF;
     END IF;
   END;
 /
