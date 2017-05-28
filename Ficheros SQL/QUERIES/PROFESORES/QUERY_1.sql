2618a5bedc00925de916ce4b6c72898ae1ac2d00
lAngelP <info@langelp.net>
2017-05-28 18:59:33 +0200

    Update QUERY_1.sql

--- Ficheros SQL/QUERIES/PROFESORES/QUERY_1.sql
+++ Ficheros SQL/QUERIES/PROFESORES/QUERY_1.sql
@@ -1,11 +1,11 @@
 SELECT COUNT(*) num, estado FROM (
 	(SELECT estado FROM aeropuerto a1
 	INNER JOIN ciudad c1 ON a1.ciudad = c1.estado
-	INNER JOIN avion a ON a2.iata = a.origen
-	INNER JOIN retrasados r ON r.vuelo = a.id)
+	INNER JOIN avion a ON a1.iata = a.numero_de_cola
+	INNER JOIN VUELOS_RETRASADOS r ON r.vuelo = a.id)
 	UNION ALL
-	(SELECT estado FROM aeropuerto a1
-	INNER JOIN ciudad c1 ON a1.ciudad = c1.estado
-	INNER JOIN avion a ON a2.iata = a.destino
-	INNER JOIN retrasados r ON r.vuelo = a.id)
+	(SELECT estado FROM aeropuerto a2
+	INNER JOIN ciudad c1 ON a2.ciudad = c1.estado
+	INNER JOIN avion a ON a2.iata = a.numero_de_cola
+	INNER JOIN VUELOS_RETRASADOS r ON r.vuelo = a.id)
 ) GROUP BY estado;
